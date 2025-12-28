927f41b8caaffb104299303a87703643ccbe706a
odukong <ohbin1017@gmail.com>
2025-12-28 16:24:45 +0900

    init : project setting

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount((prev) => prev + 1);
+        }}
+      >
+        카운트 값 : {count}
+      </button>
+    </div>
+  );
+}
+
+export default App;
